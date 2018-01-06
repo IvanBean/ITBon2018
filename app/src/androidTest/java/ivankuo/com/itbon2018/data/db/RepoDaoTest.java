6a1f4375cf7b7e72683219979ec64d352aec3087
Ivan <ivan790530@gmail.com>
2018-01-06 16:10:40 +0800

    Create RepoDaoTest to test insert and query.

---
+++ app/src/androidTest/java/ivankuo/com/itbon2018/data/db/RepoDaoTest.java
@@ -0,0 +1,49 @@
+package ivankuo.com.itbon2018.data.db;
+
+import android.arch.persistence.room.Room;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ivankuo.com.itbon2018.data.model.Owner;
+import ivankuo.com.itbon2018.data.model.Repo;
+
+import static ivankuo.com.itbon2018.util.LiveDataTestUtil.getValue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class RepoDaoTest {
+
+    private GithubDb db;
+    private Repo repo;
+
+    @Before
+    public void setUp() throws Exception {
+        db = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(),
+                GithubDb.class).build();
+
+        Owner owner = new Owner("foo", null, null);
+        repo = new Repo(1, "foo", "foo/bar", "desc", owner, 50);
+    }
+
+    @After
+    public void closeDb() throws Exception {
+        db.close();
+    }
+
+    @Test
+    public void insertAndLoad() throws InterruptedException {
+        // Insert repo
+        db.repoDao().insert(repo);
+        // Query repo
+        final Repo loaded = getValue(db.repoDao().load("foo", "foo"));
+        // Assert query result
+        assertThat(loaded.owner.login, is("foo"));
+        assertThat(loaded.name, is("foo"));
+    }
+}
